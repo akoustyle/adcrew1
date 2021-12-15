1147c7e047f2058018d00a408425c835074220e1
maoukola.oneal@gmail.com <maoukola.oneal@gmail.com>
2021-12-15 09:21:52 +0100

    finish the home and bump into the back so closing the branch

---
+++ app/controllers/talents_controller.rb
@@ -0,0 +1,51 @@
+class TalentsController < ApplicationController
+ before_action :find_talent, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @talents = Talent.all
+  end
+
+  def show
+  end
+
+  def new
+   @talent = Talent.new
+  #@talent = current_user.talents.build
+  end
+
+  def create
+    @talent = Talent.new(talent_params)
+    #@talent = current_user.talents.build(talent_params)
+    if @talent.save
+        redirect_to @talent, notice: "Yessss! It was posted"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @talent.update(talent_params)
+      redirect_to @talent, notice: "Congrats! talent was updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @talent.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def talent_params
+    params.require(:talent).permit(:name, :about, :title, :insta_link, :tiktok_link, :youtube_link)
+  end
+
+  def find_talent
+    @talent = Talent.find(params[:id])
+  end
+end
