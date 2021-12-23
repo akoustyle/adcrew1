8061b855bdd90fd19c36c0789b211708c0288e8e
O'neal Maoukola <maoukola.oneal@gmail.com>
2021-12-23 19:18:19 +0100

    fixing the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
 Talent.destroy_all
 Campaign.destroy_all
 Collab.destroy_all
 Pole.destroy_all
+
+puts "Creating poles array..."
+POLES_ARRAY = []
+POLES_ARRAY << green = Pole.create(name: 'green')
+puts "Pole: #{green.name} created !"
+POLES_ARRAY << urban = Pole.create(name: 'urban')
+puts "Pole: #{urban.name} created !"
+POLES_ARRAY << fashion = Pole.create(name: 'fashion')
+puts "Sport: #{fashion.name} created !"
+POLES_ARRAY << positivism = Pole.create(name: 'positivism')
+puts "Sport: #{positivism.name} created !"
+puts "All poles created successfully!"
 puts 'Creating talents...'
 talent_one = Talent.create(name: "O'Neal Maoukola", about: "Hello I'm a sport trainer, bump into coding during the sanitary situation, got ambition and eager to do more in that professionnal environment.", title: "coach", audience_male: "1%", audience_female: "99%", insta_link: Faker::Name.name, insta_eng: "22%", tiktok_link: Faker::Name.name, tiktok_eng: "46%", youtube_link: Faker::Name.name, youtube_eng: "73%")
 Talent.create(name: "O'Neal Maoukola", about: "Hello I'm a sport trainer, bump into coding during the sanitary situation, got ambition and eager to do more in that professionnal environment.", title: "coach", audience_male: "1%", audience_female: "99%", insta_link: Faker::Name.name, insta_eng: "22%", tiktok_link: Faker::Name.name, tiktok_eng: "46%", youtube_link: Faker::Name.name, youtube_eng: "73%")
@@ -27,15 +40,4 @@ Collab.create(talent_id: talent_one.id,  campaign_id: campaign_one.id)
 Collab.create(talent_id: talent_two.id,  campaign_id: campaign_one.id)
 Collab.create(talent_id: talent_two.id,  campaign_id: campaign_two.id)
 puts 'Done !'
-puts "Creating poles array..."
-POLES_ARRAY = []
-POLES_ARRAY << green = Pole.create(name: 'green')
-puts "Pole: #{green.name} created !"
-POLES_ARRAY << urban = Pole.create(name: 'urban')
-puts "Pole: #{urban.name} created !"
-POLES_ARRAY << fashion = Pole.create(name: 'fashion')
-puts "Sport: #{fashion.name} created !"
-POLES_ARRAY << positivism = Pole.create(name: 'positivism')
-puts "Sport: #{positivism.name} created !"
-puts "All poles created successfully!"
 # poles_ARRAY = [green, urban, fashion, positivism]
