6c9060f5ed8f1094f35f4bdc60c6b02b432a7db3
O'neal Maoukola <maoukola.oneal@gmail.com>
2021-12-24 13:39:02 +0100

    removing pole model and testing back the talent flow all is good

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -2,7 +2,6 @@ class Talent < ApplicationRecord
   has_many :collabs, dependent: :nullify
   has_many_attached :photos
   has_one_attached :video
-  belongs_to :pole
 
   # validates :photos, attached: true, limit: { min: 1, max: 3 }, content_type: [:png, :jpeg, :jpg]
   # validates :video, attached: true, content_type: [:mp3, :mp4]
