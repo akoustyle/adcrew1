1e61c6516437a50ebc6b1d8deea3697bd78e5626
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-01-04 09:54:41 +0100

    updating page prod

---
+++ app/javascript/components/gradient.js
@@ -0,0 +1,13 @@
+
+
+// const mouseMove = (event) => {
+//   windowWidth = $(window).width();
+//   windowHeight = $(window).height();
+
+//   mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+//   mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+
+//   $('.body').css('background', 'gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+// });
+
+// export { mouseMove };
