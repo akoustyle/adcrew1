72716d8cdf8fb9836fb36009e7c72c39a4767ee7
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-01-17 17:03:37 +0100

    adding font to talent show

--- app/javascript/components/gradient.js
+++ app/javascript/components/gradient.js
@@ -1,11 +1,10 @@
+// import $ from 'jquery'
+// const mouseMove = ((event) => {
+//   const windowWidth = $(window).width();
+//   const windowHeight = $(window).height();
 
-
-// const mouseMove = (event) => {
-//   windowWidth = $(window).width();
-//   windowHeight = $(window).height();
-
-//   mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
-//   mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+//   const mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+//   const mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
 
 //   $('.body').css('background', 'gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
 // });
