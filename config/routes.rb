9e13fd715a851e1ae7c3f150f70078b8ebf3ea2a
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-03-01 15:02:35 +0100

    rversing implementation of i18n to check if it push heroku still worked

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  scope '(:locale)', locale: /fr|en/ do
+  # scope '(:locale)', locale: /fr|en/ do
     root to: 'pages#home'
     resources :campaigns
     resources :talents
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     get 'contact', to: 'pages#contact', as: :contact
     get 'prod', to: 'pages#prod', as: :prod
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end
+  # end
 end
