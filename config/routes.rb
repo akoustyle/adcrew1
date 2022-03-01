85135d0dbc26bfa26d1d42b5a51a38484b7e9e33
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-03-01 11:36:39 +0100

    trying to push but got a rake task error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pages#home'
-  resources :campaigns
-  resources :talents
-  get 'team', to: 'pages#team', as: :team
-  get 'contact', to: 'pages#contact', as: :contact
-  get 'prod', to: 'pages#prod', as: :prod
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    resources :campaigns
+    resources :talents
+    get 'team', to: 'pages#team', as: :team
+    get 'contact', to: 'pages#contact', as: :contact
+    get 'prod', to: 'pages#prod', as: :prod
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
