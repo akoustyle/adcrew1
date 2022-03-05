4f73c1f6372511de55bcbb25cf7a46d6c0caf940
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-03-05 20:49:50 +0100

    translation in english done

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module AdcrewApp1
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.available_locales = [:fr, :en]
     config.i18n.default_locale = :fr
+    config.i18n.fallbacks = true
   end
 end
