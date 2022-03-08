40e6a78c0953b7eb5bef02798f2a4bff5f9bb661
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-03-08 23:50:11 +0100

    translation working done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  before_action :switch_locale
 
-  def switch_locale(&action)
-    locale = extract_locale_from_tld || I18n.default_locale
-    I18n.with_locale(locale, &action)
+  def switch_locale
+    locale = params[:locale].to_s.strip.to_sym
+	  I18n.locale = I18n.available_locales.include?(locale) ?
+	      locale :
+	      I18n.default_locale
   end
   # Get locale from top-level domain or return +nil+ if such locale is not available
   # You have to put something like:
@@ -11,9 +13,8 @@ class ApplicationController < ActionController::Base
   #   127.0.0.1 application.it
   #   127.0.0.1 application.pl
   # in your /etc/hosts file to try this out locally
-  def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
-    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 
 end
