eedaae1c5f5abd5f4094b2886479c2a52c5023ab
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-03-17 17:10:18 +0100

    adding column link to talent for link to socials media

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_224755) do
+ActiveRecord::Schema.define(version: 2022_03_17_155720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,9 @@ ActiveRecord::Schema.define(version: 2022_02_21_224755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
+    t.string "link_youtube"
+    t.string "link_insta"
+    t.string "link_tiktok"
   end
 
   create_table "users", force: :cascade do |t|
