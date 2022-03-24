02836de19fb0875702f8df55cbf75907d78ab173
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-03-24 16:53:41 +0100

    redirect to last page after authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :switch_locale
+  after_action :store_action
 
   def switch_locale
     locale = params[:locale].to_s.strip.to_sym
@@ -18,5 +19,18 @@ class ApplicationController < ActionController::Base
   # in your /etc/hosts file to try this out locally
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
   end
 end
