4eb5ea44da1469e7619c18ce80527ab6193257ea
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-04-01 14:24:27 +0200

    adding contact controller, view, model to app

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|en/ do
     resources :talents, only: [:index, :show, :new, :create, :update, :edit]
     resources :campaigns, only: [:index, :show, :new, :create, :update, :edit]
+    # resources :contacts, only: [:new, :create]
 
     # get '/:locale' => 'talents#show'
     # get '/:locale' => 'campaigns#index'
@@ -12,13 +13,14 @@ Rails.application.routes.draw do
     # resources :campaigns
     # resources :talents
     get 'team', to: 'pages#team', as: :team
-    get 'contact', to: 'pages#contact', as: :contact
+    # get 'contact', to: 'pages#contact', as: :contact
     get 'mentions', to: 'pages#mentions', as: :mentions
 
     delete '/talent/:id', to: 'talents#destroy', as: 'destroy'
     delete '/campaign/:id', to: 'campaigns#destroy', as: 'campaign_destroy'
+    post '/contacts/', to: 'contacts#create', as: 'contact'
     # get 'prod', to: 'pages#prod', as: :prod
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :contacts, only: [:new, :create]
+
   end
 end
