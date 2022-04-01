b274fb71119d8f8ae5bfe0ee56468eb14b00bed8
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-04-01 11:30:01 +0200

    adding contact controller, view, model to app

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
     delete '/campaign/:id', to: 'campaigns#destroy', as: 'campaign_destroy'
     # get 'prod', to: 'pages#prod', as: :prod
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :contacts, only: [:new, :create]
   end
 end
