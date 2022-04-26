88c10b88dff1d5ce47e2065d951b0a2600698f32
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-04-26 17:31:09 +0200

    fixing cloudinary storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -90,4 +90,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.host << "110f-80-15-44-174.eu.ngrok.io"
 end
