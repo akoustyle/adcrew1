0677badfb52a08baabd9c16ce4deefa41844fa4d
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-05-11 10:50:30 +0200

    delete grey backgroung

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,31 +1,34 @@
-class Contact < MailForm::Base
-  include MailForm::Delivery
+# class Contact < MailForm::Base
+#   include MailForm::Delivery
 
-  attribute :name,      validate: true
-  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :category
-  attribute :entreprise
-  attribute :message,    validate: true
-  attribute :nickname,  captcha: true
-  # after_create :send_email
+#   attribute :name,      validate: true
+#   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+#   attribute :category
+#   attribute :entreprise
+#   attribute :message,    validate: true
+#   attribute :nickname,  captcha: true
+#   # after_create :send_email
 
-  def persisted?
-    false
+#   def persisted?
+#     false
+#   end
+#   # Declare the e-mail headers. It accepts anything the mail method
+#   # in ActionMailer accepts.
+#   def headers
+#     {
+#       :subject => "Contact Form Inquiry from %('#{name}' <#{email})",
+#       :to => "maoukola.oneal@gmail.com",
+#       :from => "email@myallowedemailingdomain"
+#     }
+#   end
+# end
+class Contact < ApplicationRecord
+  validates :email, :name, :message, presence: true
+  after_create :send_email
+
+  private
+
+  def send_email
+    ContactMailer.with(message: self).new.deliver_now
   end
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
-  def headers
-    {
-      :subject => "Contact Form Inquiry from %('#{name}' <#{email})",
-      :to => "maoukola.oneal@gmail.com",
-      :from => "email@myallowedemailingdomain"
-    }
-  end
-  # private
-
-  # def send_email
-  #   ContactMailer.with(message: self).new.deliver_now
-  # end
-
-
 end
