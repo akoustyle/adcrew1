e7e0b8a03d856eac194eb61b026c0f8ce108c782
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-05-11 19:36:58 +0200

    pushing udpate to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope '(:locale)', locale: /fr|en/ do
+    resources :talents, param: :slug, only: [:index, :show, :new, :create, :update, :edit]
+    resources :campaigns, param: :slug, only: [:index, :show, :new, :create, :update, :edit]
+    resources :contacts, only: [:create]
     root to: 'pages#home'
     # resources :campaigns
     # resources :talents
@@ -12,9 +15,7 @@ Rails.application.routes.draw do
 
     delete '/talent/:slug', to: 'talents#destroy', as: 'destroy'
     delete '/campaign/:slug', to: 'campaigns#destroy', as: 'campaign_destroy'
-    resources :talents, param: :slug, only: [:index, :show, :new, :create, :update, :edit]
-    resources :campaigns, param: :slug, only: [:index, :show, :new, :create, :update, :edit]
-    resources :contacts, only: [:create]
+
     devise_scope :user do
       delete '/users/sign_out' => 'devise/sessions#destroy'
     end
