3e64d63fdf410f4e10d549669f70208d3e7f70e6
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-05-16 00:31:54 +0200

    repushing responsive test

---
+++ app/javascript/components/onload.js
@@ -0,0 +1,5 @@
+
+// const createImage = () => {
+//     const myElement = document.getElementById("talent-standing");
+//     myElement.style.backgroundImage = <%= cl_image_tag @talent.photos.last.key %>;
+// }
