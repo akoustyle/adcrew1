6e442c4ee300b28a904f11d86b2056fb47e1b4f5
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-05-24 19:10:45 +0200

    trying to fix the image in talent show page that is not displaying onload

--- app/javascript/components/onload.js
+++
@@ -1,5 +0,0 @@
-
-// const createImage = () => {
-//     const myElement = document.getElementById("talent-standing");
-//     myElement.style.backgroundImage = <%= cl_image_tag @talent.photos.last.key %>;
-// }
