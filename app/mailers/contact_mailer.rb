6e5cfe6fa6783c1dc9039bd57efc0ab885f1b1fd
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-06-16 10:14:19 +0200

    update contact form email direction to contact@adcrew-paris.com

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def new
     @contact = params[:contact]
-    @email = 'maoukola.oneal@gmail.com'
+    @email = 'contact@adcrew-paris.com'
 
     if Rails.env.production?
       mail_with_sendgrid_api
@@ -14,8 +14,8 @@ class ContactMailer < ApplicationMailer
 
   def mail_with_sendgrid_api
     sg = SendGrid::API.new(api_key: Rails.application.credentials.sendgrid_api_key)
-    from = SendGrid::Email.new(email: 'maoukola.oneal@gmail.com')
-    to = SendGrid::Email.new(email: 'maoukola.oneal@gmail.com')
+    from = SendGrid::Email.new(email: 'contact@adcrew-paris.com')
+    to = SendGrid::Email.new(email: 'contact@adcrew-paris.com')
     message = SendGrid::Content.new(type: 'text/html', value: @contact.formatted_message)
     mail = SendGrid::Mail.new(from, @contact.name, to, message)
     sg.client.mail._('send').post(request_body: mail.to_json)
