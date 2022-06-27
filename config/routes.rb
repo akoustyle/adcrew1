2d583695c5c524742cc32e2f9ee6ccc55429d340
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-06-27 13:35:18 +0200

    adding slash to the end of url

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope '(:locale)', locale: /fr|en/ do
-    resources :talents, param: :slug, only: [:index, :show, :new, :create, :update, :edit]
-    resources :campaigns, param: :slug, only: [:index, :show, :new, :create, :update, :edit]
-    resources :contacts, only: [:create]
+    resources :talents, param: :slug, :trailing_slash => true, only: [:index, :show, :new, :create, :update, :edit]
+    resources :campaigns, param: :slug, :trailing_slash => true, only: [:index, :show, :new, :create, :update, :edit]
+    resources :contacts,:trailing_slash => true, only: [:create]
     root to: 'pages#home'
     # resources :campaigns
     # resources :talents
     # post '/contacts/', to: 'contacts#create'
-    get 'team', to: 'pages#team', as: :team
-    get 'contact', to: 'pages#contact', as: :contact
-    get 'mentions', to: 'pages#mentions', as: :mentions
+    get 'team', to: 'pages#team', :trailing_slash => true, as: :team
+    get 'contact', to: 'pages#contact', :trailing_slash => true, as: :contact
+    get 'mentions', to: 'pages#mentions', :trailing_slash => true, as: :mentions
 
-    delete '/talent/:slug', to: 'talents#destroy', as: 'destroy'
-    delete '/campaign/:slug', to: 'campaigns#destroy', as: 'campaign_destroy'
+    delete '/talent/:slug', to: 'talents#destroy',:trailing_slash => true, as: 'destroy'
+    delete '/campaign/:slug', to: 'campaigns#destroy', :trailing_slash => true, as: 'campaign_destroy'
 
     devise_scope :user do
       delete '/users/sign_out' => 'devise/sessions#destroy'
