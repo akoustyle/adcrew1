53b27458814f7b129cc344fafcb5c9e5e55b0782
O'neal Maoukola <maoukola.oneal@gmail.com>
2022-11-05 07:17:59 +0100

    fixing databas erros

--- db/migrate/20220627062046_drop_campaign_translations.rb
+++ db/migrate/20220627062046_drop_campaign_translations.rb
@@ -1,5 +1,5 @@
 class DropCampaignTranslations < ActiveRecord::Migration[6.1]
   def change
-    drop_table :campaign_translations
+    drop_table(:campaign_translations, if_exists: true)
   end
 end
